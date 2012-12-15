1fa7519e1dec716917285d50e4e691f54883ceda
James Burke <lifesized@gmail.com>
2012-12-15 11:00:45 +0100

    some JS for mailchimp and arrows in footer and some other tweaks

--- scripts/openstate.js
+++ scripts/openstate.js
@@ -1,3 +1,7 @@
 jQuery('#announcements').slides();
 jQuery('.statements').slides();
 jQuery(".menu > ul > li > ul").parent().addClass("has-sub-menu");
+
+// adds name fields to MailChimp widget in sidebar
+jQuery('input[name="mc_mv_EMAIL"]').attr("placeholder", "Email Address");
+jQuery('input[name="mc_mv_FNAME"]').attr("placeholder", "Name");

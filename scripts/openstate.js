a0106cd317172abc16a4a8201eefd1d9db152f48
Dirk Geurs <dirklectisch@mac.com>
2012-12-21 13:22:15 +0100

    Pretty much the most dirty fix in the history of web development. Instead of re-doing the dirty CSS for the posts on the front page, I used a bit of jQuery magic to unset some properties when the posts doesn't have thumbnail. This will do for now, although it makes me cringe.

--- scripts/openstate.js
+++ scripts/openstate.js
@@ -1,3 +1,12 @@
+var noThumbPosts;
+
+noThumbPosts = jQuery(".home .post").filter(function(post) {
+  return jQuery('.entry-thumb', this).length === 0
+});
+
+jQuery(".entry-title a", noThumbPosts).css('padding-left', '0px');
+jQuery(".entry-date", noThumbPosts).css('margin-left', '0px');
+
 jQuery('#announcements').slides();
 jQuery('.statements').slides();
 
@@ -7,3 +16,4 @@ jQuery(".menu > ul > li > ul").parent().addClass("has-sub-menu");
 // adds name fields to MailChimp widget in sidebar
 jQuery('input[name="mc_mv_EMAIL"]').attr("placeholder", "Email Address");
 jQuery('input[name="mc_mv_FNAME"]').attr("placeholder", "Name");
+
